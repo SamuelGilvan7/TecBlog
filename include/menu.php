442f95b101b5068a8b60d256c2463b7a7dd2cf0b
SamuelGilvan7 <sankiragilvan@gmail.com>
2025-11-25 17:29:59 -0300

    Fim

---
+++ include/menu.php
@@ -0,0 +1,11 @@
+
+<header>
+    <h1>Tec<span>Blog</span></h1>
+<nav>
+    <a href="">Home</a>
+    <a href="">Jogos</a>
+    <a href="">Celulares</a>
+    <a href="">Informática</a>
+    <a href="">Eletrônicos</a>
+</nav>
+</header>
---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TecBlog - O seu Blog de tecnologia</title>
+    <link rel="stylesheet" href="style/estilo.css" type="text/css">
+</head>
+<body>
+    <?php include 'include/menu.php'?>
+    <!---Fim-->
+    <main>
+        
+        <div>
+            <div class="postagem">
+                <h2>Titulo da postagem 1</h2>
+                <span class="data-postagem">postado em 20 março 2022</span>
+                <img src="img/imagem1.jpg" alt="" width="620px">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo deserunt voluptatum labore! Consequatur officia voluptas, dolores minus quisquam nemo mollitia aliquam deleniti quas laborum officiis itaque ratione pariatur illo. Sint.</p>
+                <a href="">Leia mais</a>         
+            </div>
+        </div>
+        <aside>
+           <h3>Postagens recentes</h3>
+           <div><p>Lorem ipsum dolor sit amet <br>
+                    </p></div>
+            <a href="" id = "anside">Leia mais</a>
+            <hr>
+        </aside>
+        <aside>
+           <h3>Postagens recentes</h3>
+           <div><p>Lorem ipsum dolor sit amet <br>
+                    </p></div>
+            <a href="" id = "anside">Leia mais</a>
+            
+        </aside>
+
+         <aside id="links_menu">
+           <h3>Categoria</h3>
+           <a href="">Jogos</a><br>
+           <a href="">Celular</a><br>
+           <a href="">Informatica</a><br>
+           <a href="">Eletrônicos</a><br>
+           <a href="">Jogos</a>
+            
+        </aside>
+        
+        <div>
+            <div class="postagem">
+                <h2>Titulo da postagem 1</h2>
+                <span class="data-postagem">postado em 20 março 2022</span>
+                <img src="img/imagem1.jpg" alt="" width="620px">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo deserunt voluptatum labore! Consequatur officia voluptas, dolores minus quisquam nemo mollitia aliquam deleniti quas laborum officiis itaque ratione pariatur illo. Sint.</p>
+                <a href="">Leia mais</a>            
+            </div>
+        </div>
+        
+    <footer>
+    
+        <p>Todos os direitos reservados</p>
+    </footer>
+    
+    </main>
+    <!---Conteúdo principal--->
+    
+    
+</body>
+</html>
